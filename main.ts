ee2c072595b7d0631037d1b2cfdb2a3bc9509deb
Misha33356 <124496306+Misha33356@users.noreply.github.com>
2023-02-05 11:53:42 +0200

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showIcon(IconNames.Yes)
+music.playMelody("C5 F D C5 E G B A ", 120)
+basic.showArrow(ArrowNames.East)
+basic.forever(function () {
+    let list: number[] = []
+    for (let value of list) {
+        continue;
+    }
+})
